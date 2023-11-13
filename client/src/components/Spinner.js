175665b1c9e7b90b4a2ae41831e41ffa0b11b653
sachinkr24 <ujjainsingh88@gmail.com>
2023-11-13 20:40:58 +0530

    newChange

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "animate.css"; // Import the Animate.css library
 
 const Spinner = () => {
   return (
-    <div class="d-flex justify-content-center spinner">
-      <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
+    <div className="d-flex justify-content-center spinner animate__animated animate__bounce animate__rotateIn animate__spin">
+      <div className="spinner-border" role="status" style={{ color: "#ff5733" }}>
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
